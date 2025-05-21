22b4237cec83075872b6a485350b569e70677d37
AsifHossenNipun <asifhossen190@gmail.com>
2025-05-21 21:48:30 +0600

    added new allowed host

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = ['littlelemon-restaurent.onrender.com']
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'littlelemon-restaurent.onrender.com'
+                 'litlemon.onrender.com'
 ]
 
 
